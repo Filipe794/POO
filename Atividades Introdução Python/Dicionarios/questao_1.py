71228a23986399b9c8844aeef24fee888bfa6713
Filipe794 <mendesfilipe794@gmail.com>
2023-03-22 11:19:34 -0300

    POO

--- Atividades Introdução Python/Dicionarios/questao_1.py
+++ Atividades Introdução Python/Dicionarios/questao_1.py
@@ -1,13 +1,22 @@
 agenda = {}
-i = int(input('Insira a quantidade de pessoas a serem cadastradas: '))
-contatos =[]    
+i = int(input('Insira a quantidade de contatos: '))
+contatos = []
 for j in range(i):
-    agenda ={
+    contatos.append(agenda={
         "cpf": input('Insira seu cpf: '),
         "nome": input('Insira seu nome: '),
         "idade": int(input('Insira sua idade: ')),
         "telefone": input('Insira seu telefone: ')
-    }
-    contatos.append(agenda)
-
+    })
 print(contatos)
+# dic.update() - fazer um dicionario aninhado
+# contatos = {}
+# for j in range(i):
+#     contatos.update({i:{ usando o i como chave dos dicionarios
+#         "cpf": input('Insira seu cpf: '),
+#         "nome": input('Insira seu nome: '),
+#         "idade": int(input('Insira sua idade: ')),
+#         "telefone": input('Insira seu telefone: ')
+#     }
+#     })
+# print(contatos)
